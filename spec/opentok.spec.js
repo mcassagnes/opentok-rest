d98844a422a108d7518b40a84e238dea5735f52a
Dylan Jhaveri <dylanjhaveri@gmail.com>
2017-12-14 11:20:13 -0800

    add instance method opentok.rawRequest(options)
    * this allows a developer to make arbitrary authenticated requests
    to opentok endpoints
    * add option to override default baseUrl option in OpentokRequest
    * The use case for this is that tokbox always uses the same authentication
    method: 'X-OPENTOK-AUTH' header, but sometimes they put up beta apis
    at different hosts.
    
    The beta apis are unofficial, so it didn't feel right to make an entirely
    new resource, but having a handy way to make authenticated requests
    to different endpoints is very useful
    
    For practicality, this is the request we want to make from our app:
    
    https://opentok.github.io/clifton/#tag/Session-Summary%2Fpaths%2F~1project~1%7BprojectId%7D~1session~1%7BsessionId%7D%2Fget

--- spec/opentok.spec.js
+++ spec/opentok.spec.js
@@ -1,8 +1,39 @@
 /* eslint-env jasmine */
 
+const nock = require('nock')
+
 describe('Opentok module', function () {
   it('Should export a class', function () {
     const opentok = require('../lib/opentok.js')
     expect(typeof opentok).toEqual('function')
+  })
+
+  describe('rawRequest', function () {
+    const Opentok = require('../lib/opentok.js')
+
+    it('should expose a rawRequest method', function () {
+      const opentok = new Opentok('api-key-1', 'api-sec-1')
+      expect(typeof opentok.rawRequest).toEqual('function')
+    })
+
+    it('should make a request to the endpoint', function () {
+      const apiKey = 'api-key-1'
+
+      const opentok = new Opentok(apiKey, 'api-sec-1')
+      nock('https://clifton.wobbals.com', {
+        reqHeaders: {
+          'X-OPENTOK-AUTH': jasmine.any(String)
+        }
+      }).get(`/v1/project/${apiKey}/session/session-id-1`)
+        .reply(200, {projectId: apiKey, sessionId: 'session-id-1'})
+
+      return opentok.rawRequest({
+        method: 'GET',
+        baseUrl: 'https://clifton.wobbals.com/v1/project',
+        endpoint: 'session/session-id-1'
+      }).then((resp) => {
+        expect(resp).toEqual({projectId: apiKey, sessionId: 'session-id-1'})
+      })
+    })
   })
 })
