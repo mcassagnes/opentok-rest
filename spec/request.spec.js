04622ec7fd2f44ee45159e9ac6fb8d103d606a1b
Dylan Jhaveri <dylanjhaveri@gmail.com>
2017-12-14 11:29:38 -0800

    add missing test for request.send()

--- spec/request.spec.js
+++ spec/request.spec.js
@@ -2,12 +2,13 @@
 
 const OpenTokRequest = require('../lib/request')
 const TokenGenerator = require('../lib/token_generator')
+const nock = require('nock')
 
 describe('OpenTokRequest module', function () {
   let request = null
   const options = {
     type: 'post',
-    endpoint: '/test',
+    endpoint: 'test',
     body: {}
   }
 
@@ -65,6 +66,21 @@ describe('OpenTokRequest module', function () {
   })
 
   describe('send', function () {
-    pending()
+    const auth = {
+      key: 'key',
+      secret: 'secret'
+    }
+
+    it('should send a request to the backend', function () {
+      nock('https://api.opentok.com', {
+        reqHeaders: { 'X-OPENTOK-AUTH': jasmine.any(String) }
+      }).post(`/v2/project/${auth.key}/test`)
+        .reply(200, {message: 'ok'})
+
+      request.authenticate(auth)
+      return request.send().then((resp) => {
+        expect(resp).toEqual({message: 'ok'})
+      })
+    })
   })
 })
